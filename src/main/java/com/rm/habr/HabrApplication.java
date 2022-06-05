6e6c377525da10245158566ce0dc084184859f3b
ruslooob <rm952576@gmail.com>
2022-06-05 23:42:26 +0300

    Добавил еще тултипы и отсортировал стили по определенному порядку
    Мой порядок css-свойств
    Общая идея: чем больше эффект от свойства, тем выше оно в стилях
    
    Более развернутое описание
    outer styles
    display styles (global-impact-styles)
    property-styles (h, w, p)
    margins
    background-styles
    font-styles
    border-styles
    other
    
    Порядок
    display, object-fit, position
    flex(grid)-styles fd, jcc, aic, gap, fw (ИСключение, если свойства флекс связаны как-то с контейнером, то они первее display: flex); Position styles (top, left, bottom, right)
    width, height
    margin
    padding
    background, background-color
    font-styles: font, fz, color, lh, fz
    border
    cursor
    outline

--- src/main/java/com/rm/habr/HabrApplication.java
+++ src/main/java/com/rm/habr/HabrApplication.java
@@ -6,11 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HabrApplication {
     /* todo сделать адаптив*/
-    /* todo make tooltips over icons*/
     /* todo сделать бан пользователей*/
     /* todo сделать валидацию на клиенте*/
     /* todo сделать профиль пользователя, чтобы можно было узнать под чьим именем зашел*/
     /* todo сделать так в бд, чтобы нельзя было удалить пользователя, если у него остались еще публикации*/
+    /* todo сделать локализацию */
+    /* todo сделать api для комментариев */
     public static void main(String[] args) {
         SpringApplication.run(HabrApplication.class, args);
     }
