44c0fe651722c7f5d8b2a1465eb6c7c73f0ea944
dimon <dimonbubyr@mail.ru>
2023-03-22 21:06:33 +0300

    Убрал репозитории

---
+++ src/main/java/com/rm/habr/service/TagService.java
@@ -0,0 +1,23 @@
+package com.rm.habr.service;
+
+import com.rm.habr.model.Tag;
+import com.rm.habr.repository.TagRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TagService {
+
+    private final TagRepository tagRepository;
+
+    @Autowired
+    public TagService(TagRepository tagRepository) {
+        this.tagRepository = tagRepository;
+    }
+
+    public List<Tag> findAll() {
+        return tagRepository.findAll();
+    }
+}
